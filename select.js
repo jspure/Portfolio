d1f70bcb34aea69680d70b2a88308e8fc46a3d9c
Ctyode <ctyode@gmail.com>
2017-08-22 18:33:08 +0300

    Heroes never die

---
+++ select.js
@@ -0,0 +1,73 @@
+$(".tabs ul li").click(function() {
+
+    var tab = $(this).index();
+    var position = 210 * tab;
+
+    if($(this).hasClass('slider')) {
+        return;
+    }
+
+    $(".slider").css({
+        left: position + "px"
+    });
+
+    $('li').removeClass('active');
+    $(this).addClass('active');
+    $(this).find('li').addClass('active');
+});
+
+if($) {
+
+    $('#container > *').hide(0);
+    $('#container #interfaces').show(0);
+
+    $(".menuElement").click(function() {
+
+        $('#container > *').hide(0)
+
+        var tabID = $(this).data("tab");
+        $('#' + tabID).show(0);
+
+    });
+}
+
+$(function() {
+  $(".overlay").each(function(_, overlay) {
+    var $overlay = $(overlay);
+    var $carousel = $overlay.find(".carousel");
+    var $content = $overlay.find(".content");
+    var $ul = $carousel.find("ul");
+
+    $carousel.find(".right_arrow").click(function() {
+      var $first = $ul.find("li").first();
+
+      $ul.animate({
+        left: -222
+      }, 250, function() {
+        $first.detach();
+        $ul.append($first);
+        $ul.css("left", 0);
+      });
+    });
+
+    $carousel.find(".left_arrow").click(function() {
+      var $last = $ul.find("li").last();
+
+      $ul.css("left", -222);
+      $last.detach();
+      $ul.prepend($last);
+      $ul.animate({
+        left: 0
+      }, 250)
+    });
+
+    $ul.find("li").each(function(_, li) {
+
+      var $li = $(li);
+      $li.click(function() {
+        $content.find(".info").removeClass("active");
+        $content.find(".info[data-info-id=" + $li.attr("data-info-id") + "]").addClass("active");
+      });
+    });
+  });
+});
